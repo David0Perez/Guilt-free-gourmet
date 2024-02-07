09919201ae91a1b3278bb00a055e4ad3b28288c2
Mafer Angulo <maferadr@Mafers-Air.lan>
2024-02-07 09:37:28 -0500

    Session settings were set up. Routes working properly

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const session  = require("express-session")
+const SequelizeStore = require("connect-session-sequelize")(session.Store)
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
@@ -8,7 +10,24 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {
+      maxAge: 300000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+
 const hbs = exphbs.create({});
+
+app.use(session(sess))
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -21,5 +40,5 @@ app.use(routes);
 
 sequelize.sync({ force: false}).then(()=>{
     app.listen(PORT, () => console.log(`App listening to PORT ${PORT}`));
-    
+
 })
