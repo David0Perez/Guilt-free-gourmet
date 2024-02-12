ff5bab6862c212f613f6d43e5213713fefcf8f54
Mafer Angulo <maferadr@Mafers-Air.lan>
2024-02-12 12:08:10 -0500

    User verification is working properly. Need to add styling to display recipes on the screen for the API request

--- public/js/login.js
+++ public/js/login.js
@@ -18,7 +18,7 @@ const loginFormHandler = async (e) =>{
 
         //We double check if the response is successful => It will give the user access to the profile
         if(response.ok){
-            document.location.replace('/userProfile');
+            document.location.replace('/profile');
         }else{
             alert(response.statusText);
         }
@@ -29,27 +29,30 @@ const loginFormHandler = async (e) =>{
 const signUpForm = async (e) =>{
     e.preventDefault();
 
-    const name = document.querySelector('#nameLogin').value.trim();
+    const username = document.querySelector('#nameLogin').value.trim();
     const email = document.querySelector('#signUpEmail').value.trim();
     const password = document.querySelector('#signUpPass').value.trim();
 
     //Conditional for values entered of each one
-    if(email && password && name){
+    if(email && password && username){
         const response = await fetch('/api/users', {
             method: 'POST',
-            body: JSON.stringify({ name, email, password}),
+            body: JSON.stringify({ username, email, password}),
             headers: { 'Content-Type': 'application/json'},
         });
+
+         //Redirect to the profile
+        if(response.ok){
+            //sweet alert asking you to log in your information
+            alert('You already Signed Up! Log in your account.')
+            // document.location.replace('/userProfile');
+        }else{
+            alert(response.statusText);
+        }
     };
     
-    //Redirect to the profile
-    if(response.ok){
-        document.location.replace('/userProfile');
-    }else{
-        alert(response.statusText);
-    }
 };
 
-//We call for both functions and assign a submit event on the buttons.
+//We call for both functions and assign a submit event on the buttons. => Currently assign on the form, not the buttons
 document.querySelector('.signUpForm').addEventListener('submit', signUpForm);
 document.querySelector('.logInForm').addEventListener('submit', loginFormHandler);
