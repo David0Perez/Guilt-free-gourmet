d086f62b9107a427a0dcc2543fb6382d48da6a14
Mafer Angulo <maferadr@Mafers-Air.lan>
2024-02-12 17:37:00 -0500

    README file added

--- public/js/profile.js
+++ public/js/profile.js
@@ -3,16 +3,16 @@ const newRecipeForm = async (e) =>{
     e.preventDefault();
 
     // recipe_name, ingredients, directions
-    const recipeName = document.querySelector('#recipe-name').value.trim();
-    const recipeIngredients = document.querySelector('#recipe-ingredients').value.trim();
-    const recipeDirections = document.querySelector('#recipe-directions').value.trim();
+    const recipe_name = document.querySelector('#recipe-name').value.trim();
+    const ingredients = document.querySelector('#recipe-ingredients').value.trim();
+    const description = document.querySelector('#recipe-directions').value.trim();
 
     //Bear in mind => Recipes created are just gonna be displayed on a profile section
     // Recipes requested by a third party API are gonna show up on a Explore Recipes section
-    if(recipeName && recipeIngredients && recipeDirections){
+    if(recipe_name && ingredients && description){
         const response = await fetch('/api/recipes', {
             method: 'POST',
-            body: JSON.stringify( { recipeName, recipeIngredients, recipeDirections }),
+            body: JSON.stringify( { recipe_name, ingredients, description }),
             headers: {
                 'Content-Type': 'application/json',
             }
@@ -42,14 +42,40 @@ const newCommentHandler = async (e) =>{
         });
 
         if(response.ok){
-            document.location.replace('/userProfile');
+            document.location.replace('/profile');
         }else{
             alert('Couldnt Post this comment. Please Try again!')
         }
     }
 };
 
+//Find recipe 
+const findRecipe = async (e) =>{
+    e.preventDefault();
+
+    const searchFavorite = document.querySelector('#searchFavorite').value.trim();
+
+    if(searchFavorite){
+        const response = await fetch('/api/recipes', {
+            method: 'POST',
+            body: JSON.stringify({ searchFavorite }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if(response.ok){
+            document.location.replace('/recipe/:id');
+        }else{
+            console.log('Error');
+        }
+    }
+};
+
 document.querySelector('#createBtn').addEventListener('submit', newRecipeForm);
 document.querySelector('#submitComment').addEventListener('submit', newCommentHandler);
+
+//Find recipe button function
+document.querySelector('#findRecipeBtn').addEventListener('submit', findRecipe);
 
 // For further implementations => Delete btn
